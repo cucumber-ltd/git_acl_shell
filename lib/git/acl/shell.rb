7840aaa5fe0fc5681b9ac5597d17a33c8f937198
Steve Tooke <tooky@cucumber.io>
2017-02-09 11:27:23 +0000

    First post!

---
+++ lib/git/acl/shell.rb
@@ -0,0 +1,9 @@
+require "git/acl/shell/version"
+
+module Git
+  module Acl
+    module Shell
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/git/acl/shell/version.rb
@@ -0,0 +1,7 @@
+module Git
+  module Acl
+    module Shell
+      VERSION = "0.1.0"
+    end
+  end
+end
