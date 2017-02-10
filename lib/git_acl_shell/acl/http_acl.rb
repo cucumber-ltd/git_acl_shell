0bda3782b3d5bd5536db447c29c1c562b743470c
Julien Biezemans <jb@jbpros.com>
2017-02-10 11:37:22 +0100

    Let HTTParty URL-encode all the things

--- lib/git_acl_shell/acl/http_acl.rb
+++ lib/git_acl_shell/acl/http_acl.rb
@@ -6,8 +6,7 @@ module GitAclShell
       include HTTParty
 
       def authorized?(key_id, repo_alias)
-        # TODO: URL encode
-        response = self.class.get("/permission?keyId=#{key_id}&alias=#{repo_alias}")
+        response = self.class.get("/permission", query: { keyId: key_id, alias: repo_alias })
         response.ok?
       end
     end
--- lib/git_acl_shell/directory/http_directory.rb
+++ lib/git_acl_shell/directory/http_directory.rb
@@ -6,8 +6,7 @@ module GitAclShell
       include HTTParty
 
       def lookup(repo_alias)
-        # TODO: URL encode
-        response = self.class.get("/real-name?alias=#{repo_alias}")
+        response = self.class.get("/real-name", query: { alias: repo_alias })
         if response.ok?
           return response.body
         else
